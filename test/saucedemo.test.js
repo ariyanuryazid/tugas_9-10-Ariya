b07b588b0e5868194aa373955ab3658f88f47fc6
ariyanuryazid <149025896+ariyanuryazid@users.noreply.github.com>
2025-04-21 19:48:36 +0700

    Update saucedemo.test.js

--- test/saucedemo.test.js
+++ test/saucedemo.test.js
@@ -3,25 +3,33 @@ const firefox = require('selenium-webdriver/firefox');
 const assert = require('assert');
 
 describe('SauceDemo Automation Test', function () {
-  this.timeout(30000); // Biar nggak timeout
+  this.timeout(30000); // Set waktu maksimal tiap test 30 detik
 
   let driver;
 
   before(async () => {
     driver = await new Builder().forBrowser('firefox').build();
-  });
 
-  after(async () => {
-    await driver.quit();
-  });
-
-  it('Login sukses ke saucedemo.com', async () => {
+    // Login sekali 
     await driver.get('https://www.saucedemo.com/');
-
     await driver.findElement(By.id('user-name')).sendKeys('standard_user');
     await driver.findElement(By.id('password')).sendKeys('secret_sauce');
     await driver.findElement(By.id('login-button')).click();
 
+    // masuk halaman inventory
+    await driver.wait(until.urlIs('https://www.saucedemo.com/inventory.html'), 5000);
+  });
+
+  // Setelah semua test 
+  after(async () => {
+    await driver.quit();
+  });
+
+  beforeEach(async () => {
+    await driver.get('https://www.saucedemo.com/inventory.html');
+  });
+
+  it('Berhasil login ke saucedemo.com', async () => {
     const url = await driver.getCurrentUrl();
     assert.strictEqual(url, 'https://www.saucedemo.com/inventory.html');
   });
@@ -35,7 +43,9 @@ describe('SauceDemo Automation Test', function () {
       5000
     );
     const itemName = await firstItem.getText();
-    
+
     console.log('Produk paling atas setelah Z-A sort:', itemName);
+    assert.ok(itemName); // Minimal pastikan itemName tidak kosong
   });
 });
+
